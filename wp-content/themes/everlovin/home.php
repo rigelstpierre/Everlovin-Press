df11e7ed9b8925dc71f6b5ae280f1b4e8e5f8bdc
Rigel St. Pierre <rigelstp@gmail.com>
2012-04-05 11:03:44 -0700

    completes homepage loop

--- wp-content/themes/everlovin/home.php
+++ wp-content/themes/everlovin/home.php
@@ -20,7 +20,7 @@ Template Name: Home
 					<a class="texture-1" href="<? the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
 		        <?php endwhile; wp_reset_postdata(); ?>
 				
-				<?php $the_query = new WP_Query( array('post_type' => array('work'),'showposts' => '1' ));
+				<?php $the_query = new WP_Query( array('post_type' => array('work'),'showposts' => '1', 'offset' => '1' ));
 		          while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 		            <div class="smallbox orange">
 						<?php the_post_thumbnail(); ?>
@@ -28,12 +28,12 @@ Template Name: Home
 					<a class="texture-2" href="<? the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
 		        <?php endwhile; wp_reset_postdata(); ?>
 				
-				<?php $the_query = new WP_Query( array('post_type' => array('work'),'showposts' => '1' ));
+				<?php $the_query = new WP_Query( array('post_type' => array('work'),'showposts' => '1', 'offset' => '2' ));
 		          while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 		            <a class="largebox orange" href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
 		        <?php endwhile; wp_reset_postdata(); ?>
 		       
-		        <?php $the_query = new WP_Query( array('post_type' => array('work'),'showposts' => '1' ));
+		        <?php $the_query = new WP_Query( array('post_type' => array('work'),'showposts' => '1', 'offset' => '3' ));
 		          while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 		            <div class="smallbox white" >
 						<? the_post_thumbnail(); ?>
