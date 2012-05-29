f9b0fb17cb71c12f96a530846bcff11f7feb8028
Rigel St. Pierre <rigelstp@gmail.com>
2012-05-28 21:09:45 -0700

    first commit in awhile, adding new fav.ico

--- wp-content/themes/everlovin/single.php
+++ wp-content/themes/everlovin/single.php
@@ -4,9 +4,9 @@
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<section class="single">
 					<aside>
-						<div>
-							<?php echo get_post_type( $post->ID ); ?>
-						</div>
+						<span class="prev"><?php next_post('%', 'Next', 'no'); ?></span>
+						<div><?php echo get_post_type( $post->ID ); ?></div>
+						<span class="next"><?php previous_post('%','Previous', 'no'); ?></span>
 						<h3><?php the_title(); ?></h3>
 						<h2>the low-down</h2>
 						<p><?php the_field('low_down'); ?></p>
@@ -21,6 +21,39 @@
 					</aside>
 					<?php the_content(); ?>
 				</section>
+				<section class="work">
+				<?php $the_query = new WP_Query( array('post_type' => array('customwork'),'showposts' => '1' ));
+		          while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+		        	<div class="smallbox blue">
+						<? the_post_thumbnail(); ?>
+					</div>
+					<a class="texture-1" href="<? the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+		        <?php endwhile; wp_reset_postdata(); ?>
+				
+				<?php $the_query = new WP_Query( array('post_type' => array('customwork'),'showposts' => '1', 'offset' => '1' ));
+		          while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+					<div class="smallbox white">
+						<? the_post_thumbnail(); ?>
+					</div>
+					<a class="texture-3" href="<? the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+		        <?php endwhile; wp_reset_postdata(); ?>
+				
+				<?php $the_query = new WP_Query( array('post_type' => array('customwork'),'showposts' => '1', 'offset' => '2' ));
+		          while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+		            <div class="smallbox orange">
+						<?php the_post_thumbnail(); ?>
+					</div>
+					<a class="texture-2" href="<? the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+		        <?php endwhile; wp_reset_postdata(); ?>
+		       
+		        <?php $the_query = new WP_Query( array('post_type' => array('customwork'),'showposts' => '1', 'offset' => '3' ));
+		          while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+		           	<div class="smallbox blue">
+						<? the_post_thumbnail(); ?>
+					</div>
+					<a class="texture-4" href="<? the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+		        <?php endwhile; wp_reset_postdata(); ?>
+			</section>
 				<?php endwhile; endif; ?>
 			</div><!-- end container -->
 		</div><!-- end main -->
